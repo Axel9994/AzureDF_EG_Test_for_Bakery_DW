7cd6985d72fb6d3b2583dfa1b547b74f6ab2b31f
Axel Aleman <axelalemanyi@hotmail.com>
2024-02-11 19:46:31 -0500

    Primer Commit

---
+++ create dimensions table.sql
@@ -0,0 +1,58 @@
+if not exists (select * from sysobjects where name='DimCustomer' and xtype='U')
+    create table DimCustomer (
+        Customer_ID int,
+		customer_name varchar(50)
+    );
+if not exists (select * from sysobjects where name='DimProduct' and xtype='U')
+    create table DimProduct (
+        Product_ID int,
+		product_name varchar(50),
+		product_category varchar(50)
+    );
+if not exists (select * from sysobjects where name='DimPaymentMethod' and xtype='U')
+    create table DimPaymentMethod (
+        PaymentMethod_ID int,
+		payment_method varchar(50)
+    );
+if not exists (select * from sysobjects where name='DimLocation' and xtype='U')
+    create table DimLocation (
+        Location_ID int,
+		delivery_address varchar(50),
+		city varchar(50),
+		country varchar(50)
+    );
+truncate table DimCustomer;
+truncate table DimProduct;
+truncate table DimPaymentMethod;
+truncate table DimLocation;
+insert into DimCustomer
+select ROW_NUMBER() OVER (ORDER BY c.customer_name) as Customer_ID,
+	  c.customer_name
+from (
+ select distinct customer_name
+ from dbo.stage_bakery
+) c;
+insert into DimProduct
+select ROW_NUMBER() OVER (ORDER BY p.product_name,
+	  p.product_category) as Product_ID,
+	  p.product_name,
+	  p.product_category
+from (
+ select distinct product_name, product_category
+ from dbo.stage_bakery
+) p;
+insert into DimPaymentMethod
+select ROW_NUMBER() OVER (order by pp.payment_method) as PaymentMethod_ID,
+pp.payment_method
+from (
+select distinct payment_method
+from dbo.stage_bakery
+) pp;
+insert into DimLocation
+select ROW_NUMBER() OVER (ORDER BY delivery_address, city, country) as Location_ID,
+delivery_address,
+city,
+country
+from
+(select distinct delivery_address, city, country
+from dbo.stage_bakery) l;
---
+++ create fact sales table.sql
@@ -0,0 +1,31 @@
+if not exists (select * from sysobjects where name='FactSales' and xtype='U')
+	create table FactSales(
+	 SalesID int identity(1,1),
+	 Customer_ID int,
+	 Product_ID int,
+	 PaymentMethod_ID int,
+	 Location_ID int,
+	 quantity int,
+	 unit_price decimal(10,2),
+	 discount decimal(10,2),
+	 total_price decimal(10,2)
+	 );
+truncate table dbo.FactSales;
+ insert into FactSales
+ select c.Customer_ID,
+        p.Product_ID,
+		pp.PaymentMethod_ID,
+		l.Location_ID,
+		s.quantity,
+		s.unit_price,
+		s.discount,
+		s.total_price
+from dbo.stage_bakery s
+left join dbo.DimCustomer c
+on s.customer_name = c.customer_name
+left join dbo.DimProduct p
+on s.product_name = p.product_name and s.product_category = p.product_category
+left join dbo.DimPaymentMethod pp
+on s.payment_method = pp.payment_method
+left join dbo.DimLocation l
+on s.delivery_address = l.delivery_address and s.city = l.city and s.country = l.country;
